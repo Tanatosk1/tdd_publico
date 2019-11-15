14b46b1185f0c09d373fc5529f777516711e4950
David Ramallo <alu0101061320@ull.edu.es>
2019-11-15 12:19:39 +0000

    Creamos pruebas para impacto ambiental con estado correcto

--- lib/alimentos/alimentos.rb
+++ lib/alimentos/alimentos.rb
@@ -1,5 +1,5 @@
 class Alimento
-   attr_reader :nombre, :gei, :terreno
+   attr_reader :nombre, :gei, :terreno, :proteinas
    def initialize(nombre, proteinas, carbohidratos, lipidos, gei, terreno)
     @nombre = nombre
     @proteinas = proteinas
@@ -14,4 +14,27 @@ class Alimento
    def valorEnergetico
 	   	(@proteinas*4 + @carbohidratos*4 + @lipidos*9).round(4)
    end
+   def impactoAmbiental(lista_alimentos, lista_cantidades, genero) #genero es para saber si nos referimos a hombre (0) o mujer (1)
+			gei = 0
+	                terreno = 0
+			proteinas = 0
+			kcal = 0
+			cont = 0
+			lista_alimentos.each do |alimento|  #Recorrer la lista de alimentos extrayendo las kcal de cada uno
+				kcal = (kcal + alimento.valorEnergetico*lista_cantidades[cont]).round(4)
+				gei = (gei + alimento.gei*lista_cantidades[cont]).round(4) #Incrementar GEI de cada alimento
+				terreno = (terreno + alimento.terreno*lista_cantidades[cont]).round(4) #Incrementar terreno de cada alimento
+				proteinas = proteinas + alimento.proteinas
+				cont = cont +1
+			end
+	
+			if genero == 0 && kcal >= 3000 && proteinas >= 54
+				return gei + terreno
+			
+			elsif genero == 1 && kcal >= 2300 && proteinas >= 41
+					return gei + terreno
+			else
+				"ERROR"
+			end
+    end	
 end
