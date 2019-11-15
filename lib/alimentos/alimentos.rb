de1ea2e75e85ce76119342b584911830d3bd40b0
David Ramallo <alu0101061320@ull.edu.es>
2019-11-15 00:34:31 +0000

    def to_s Nombre:

--- lib/alimentos/alimentos.rb
+++ lib/alimentos/alimentos.rb
@@ -1,7 +1,11 @@
 class Alimento
+   attr_reader:nombre
    def initialize(nombre, proteinas, carbohidratos, lipidos, gei, terreno)
     @nombre = nombre
     @gei = gei
     @terreno = terreno    
+   end
+   def to_s
+	   "Nombre: #{@nombre}."
    end
 end
